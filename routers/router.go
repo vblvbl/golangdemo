b4ff635991d8daee76095433443b175db38b3b71
谢权 <i@xiequan.info>
2015-05-30 20:14:45 +0800

    simpe mvc

--- routers/router.go
+++ routers/router.go
@@ -3,36 +3,28 @@ import (
 	"net/http"
 
 	"golangdemo/controllers"
-
 )
 
 
 type MyMux struct {
 }
 
+
+
 func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.RequestURI
-	var resp string
-	switch path {
-	case "/hello":
-		resp = "hello"
-	case "/haha":
-		resp = "===="
-	default:
-		resp = "/"
+	if r.URL.Path == "/" {
+		contollers.SayhelloName(w,r)
+		return
 	}
-	if resp == "/hello" {
-		contollers.SayhelloName(w, r)
-	}
-
-
 	http.NotFound(w, r)
 	return
 }
 
 
+
+
 func Init() {
 	mux := &MyMux{}
-	http.ListenAndServe("9090", mux)
+	http.ListenAndServe(":8080", mux)
 
 }
