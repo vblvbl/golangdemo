ec5eaa8aa062494abd1fdc7433cfb45898219a3e
谢权 <i@xiequan.info>
2015-12-09 11:44:13 +0800

    add md5 and stringutil

---
+++ utils/md5util.go
@@ -0,0 +1,11 @@
+package utils
+import (
+	"encoding/hex"
+	"crypto/md5"
+)
+//md5方法
+func GetMd5String(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
---
+++ utils/stringutil.go
@@ -0,0 +1,25 @@
+package utils
+import (
+	"encoding/base64"
+	"io"
+	"crypto/rand"
+)
+
+//字串截取
+func SubString(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+//Guid方法
+func GetGuid() string {
+	b := make([]byte, 48)
+
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		return ""
+	}
+	return GetMd5String(base64.URLEncoding.EncodeToString(b))
+}
