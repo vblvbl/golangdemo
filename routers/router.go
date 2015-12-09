a554850bc306c9f0b58041a8f0f506bea8aa4914
谢权 <i@xiequan.info>
2015-12-09 11:24:42 +0800

    update router add logpaincs

--- routers/router.go
+++ routers/router.go
@@ -1,23 +1,43 @@
 package routers
 
 import (
+	"fmt"
 	"golangdemo/controllers"
 	"golangdemo/core"
 	"golangdemo/middlewares"
+	"net/http"
+
+	"github.com/go-errors/errors"
 )
 
 // getRouter returns the routers
 func GetRouter() (router *core.Router) {
 	router = core.NewRouter()
-
 	// All routes go here
-	router.HandleFunc("/", controllers.SayhelloName)
-
+	router.HandleFunc("/", LogPaincs(controllers.SayhelloName))
 	//Static Controller
 	router.PathPrefix("/").Handler(&controllers.Static{"/static/public", router})
-
 	// All middlewares go here
 	router.AddMiddleware("/", &middlewares.HTTPLogger{})
 
 	return
+}
+
+type MyError struct {
+	Msg interface{}
+}
+
+func LogPaincs(function func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if x := recover(); x != nil {
+				myError := &MyError{}
+				myError.Msg = errors.Wrap(x, 2).ErrorStack()
+				errstr := fmt.Sprintf("%v", myError.Msg)
+				fmt.Println(errstr)
+
+			}
+		}()
+		function(w, r)
+	}
 }
